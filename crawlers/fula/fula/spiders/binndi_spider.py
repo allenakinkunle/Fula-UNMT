1fcf14d30498344270f9e36b96a8840f1a7e3aaf
allenakinkunle <hello@allenkunle.me>
2020-05-03 22:11:50 +0100

    Add crawler for binndipulaar.com website

---
+++ crawlers/fula/fula/spiders/binndi_spider.py
@@ -0,0 +1,21 @@
+import json
+import os
+import scrapy
+from scrapy.http import FormRequest
+
+class BinndiSpider(scrapy.Spider):
+    name = 'binndi'
+    
+    with open('fula/spiders/binndi_link.txt', 'r') as f:
+        start_urls = f.readlines()
+
+    def parse(self, response):
+        date = response.css('time::text').get()
+        url = response.url
+    
+        for paragraph in response.css('p.has-text-align-justify::text'):
+            yield {
+                'text': paragraph.get().strip(),
+                'date': date,
+                'url': url
+            }
